417229ccdd26c2760e29a20a457da04bedc149a3
Vinicius Ortega <51245018+viniciusortega@users.noreply.github.com>
2021-10-25 18:44:17 -0300

    add VPC in module

--- instances/main.tf
+++ instances/main.tf
@@ -2,3 +2,11 @@ resource "aws_instance" "foo" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t3.micro"
 }
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
